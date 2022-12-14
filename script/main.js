0596e12c14b59ddd27a4fa6a6b19cea42ea99030
yurledyspuche25 <89873596+yurledyspuche25@users.noreply.github.com>
2022-12-14 09:25:44 -0500

    added style

--- script/main.js
+++ script/main.js
@@ -1,7 +1,16 @@
- let countDownDate = new Date ("sept 09, 1996, 00: 00: 00").getTime();
+ let countDownDate = new Date ("12 18, 2022, 00: 00: 00").getTime();
  let x = setInterval(function(){
     let now  = new Date().getTime();
     let distance = countDownDate - now;
 
-    let days =Math.floor(distance/(1000))
+    let days =Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60 ));
+    let minutes =Math.floor((distance % (1000 * 60 * 60 * 60)) / (1000 * 60  ));
+    let seconds =Math.floor((distance % (1000 * 60 )) / (1000 ));
+
+    document.getElementById('days').innerHTML = days;
+    document.getElementById('hours').innerHTML = hours;
+    document.getElementById('minutes').innerHTML = minutes;
+    document.getElementById('seconds').innerHTML = seconds;
+
  },1000);
